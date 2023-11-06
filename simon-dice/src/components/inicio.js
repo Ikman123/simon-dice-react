546465383347fcd00f61617fb12ed6d17ef5905c
Ikman123 <88504533+Ikman123@users.noreply.github.com>
2023-11-06 12:28:16 -0300

    segundo commit

--- simon-dice/src/components/inicio.js
+++ simon-dice/src/components/inicio.js
@@ -1,16 +1,34 @@
 import './styles.css';
+import React, { useState } from 'react';
 
 function Inicio() {
-    return (
-    <>
-        <div className='cont-nombre'>
-            <label><h3>Ingresa tu nombre:</h3></label>
-            <input></input>
-            <button>guardar</button>
-        </div>
-        <button className='iniciar'>INICIAR JUEGO</button>
+    const [name, setName] = useState('');
+    const [savedName, setSavedName] = useState('');
 
-    </>
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleGuardarClick = () => {
+        setSavedName(name);
+    };
+
+    return (
+        <>
+            <div className='cont-nombre'>
+                <form>
+                    <label>Ingresa tu nombre:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                    <button onClick={handleGuardarClick}>Guardar</button>
+                </form>
+            </div>
+            <button className='iniciar'>INICIAR JUEGO</button>
+            {savedName && <p>Tu nombre guardado: {savedName}</p>}
+        </>
     );
 }
 
